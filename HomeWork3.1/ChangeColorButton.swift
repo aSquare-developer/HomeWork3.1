9ce8a158bb8f55e4fab03ea8f90b4e2f687f47d2
aSquare <artur.anissimov@gmail.com>
2022-01-25 19:38:04 +0200

    Code refactoring Create new View(Button)

---
+++ HomeWork3.1/ChangeColorButton.swift
@@ -0,0 +1,36 @@
+//
+//  ChangeColorButton.swift
+//  HomeWork3.1
+//
+//  Created by Artur Anissimov on 25.01.2022.
+//
+
+import SwiftUI
+
+struct ChangeColorButton: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.white, lineWidth: 4)
+        )
+        
+    }
+}
+
+struct ChangeColorButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeColorButton(title: "START", action: {})
+    }
+}
